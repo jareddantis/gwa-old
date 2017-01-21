7cda06ef9264992aa75e4373e2b38d98e39ee672
Aurel Jared Dantis <jareddantis@gmail.com>
2017-01-21 22:19:58 +0800

    Release 3
    - New design
    - Show batches instead of grade levels
    - GWA is now pinned to top of page (yay mobile users!)
    - Night mode
    - Quick grades clearing
    - Input boxes are now of number type, so they will automatically bring
    up the number keypad on mobile devices

--- js/app.js
+++ js/app.js
@@ -1,22 +1,51 @@
 $(document).ready(function(){
+	// Restore state
 	grade.default = grade.seven;
 	state.load();
+
+	// Populate dropdown list and make it work
 	$("li").each(function(el){
 		$(this).click(function(e){
 			var that = $(this).children();
 			e.preventDefault();
-			state.update($(that).attr('data-item'), $(that).text(), null);
+			state.update($(that).attr('data-item'), null);
 		})
+		var di = levelToBatch($(this).children().attr("data-item"));
+		$(this).children().text("Batch " + di);
 	});
+
+	// Night mode
+	$('#c_nm').click(function(){
+		var isNight = $('html').hasClass('night');
+		state.current.isNightMode = !isNight;
+		state.save();
+		return isNight ? $('html').removeAttr('class') : $('html').addClass('night');
+	});
+
+	// Menu button animation
+	$('#hamburger').click(function(){
+		if ($(this).hasClass('active')) {
+			$(this).removeAttr('class');
+			$('#menu .menu-content').addClass('invis');
+		} else {
+			$(this).addClass('active');
+			$('#menu .menu-content').removeClass('invis');
+		}
+	})
+
+	// Clear button
+	$('#refresh').click(redraw);
 });
 
 var state = {
 	current: {
 		set: null,
-		grades: []
+		grades: [],
+		isNightMode: false
 	},
-	update: function(newSet, newName, redrawValues) {
+	update: function(newSet, redrawValues) {
 		if (grade[newSet]) {
+			var newName = levelToBatch(newSet);
 			$("#btn-text").text(newName);
 			grade.default = grade[newSet];
 			grade.set = newSet;
@@ -47,17 +76,11 @@ var state = {
 		if (typeof(Storage) !== undefined) {
 			var savedJson = localStorage.getItem("gwadata");
 			if (savedJson != null) {
-				var savedState = JSON.parse(savedJson),
-					savedSet = savedState.set, savedName, savedGrades = savedState.grades;
+				var savedState = JSON.parse(savedJson), nightMode = savedState.isNightMode,
+					savedSet = savedState.set, savedGrades = savedState.grades;
 				this.current = savedState;
-				switch (savedSet) {
-					case "seven": savedName = "Grade 7"; break;
-					case "eight": savedName = "Grade 8"; break;
-					case "nine": savedName = "Grade 9"; break;
-					case "ten": savedName = "Grade 10"; break;
-					case "tweleven": savedName = "Grade 11 & 12"; break;
-				}
-				this.update(savedSet, savedName, savedGrades);
+				this.update(savedSet, savedGrades);
+				if (nightMode) { $('html').addClass('night') }
 			} else
 				redraw();
 		}
@@ -70,6 +93,22 @@ var state = {
 			console.log("saved " + currentJson);
 		}
 	}
+}
+
+function levelToBatch(level) {
+	var currYear = (new Date()).getFullYear(), batch, tweleven;
+	if (currYear == 2017) {
+		tweleven = currYear + 1;
+	} else
+		tweleven = (currYear + 1) + " & " + currYear;
+	switch (level) {
+		case "seven": batch = currYear + 5; break;
+		case "eight": batch = currYear + 4; break;
+		case "nine": batch = currYear + 3; break;
+		case "ten": batch = currYear + 2; break;
+		case "tweleven": batch = tweleven; break;
+	}
+	return batch;
 }
 
 var grade = {
@@ -134,58 +173,83 @@ var grade = {
 };
 
 function redraw(values) {
+	//return;
 	$("input").each(function(){ $(this).detach() });
-	$('#left').empty();
-	$('#right').empty();
-	$("#g").attr("class","").text("GWA: (incomplete data)");
-
-	for (var i = 0; i < 6; i++) {
-		var fg = $("<div></div>").addClass("form-group");
-		var label = $("<label></label>").addClass("control-label");
-		var box = $("<input>").addClass("form-control")
-		            .attr("type", "text").attr("data-subject", i)
-		            .attr("maxlength","4");
-		if (values != null) { $(box).val(values[i]) }
-		$(label).text(grade.default[i].subject);
-		$(fg).append(label).append(box);
-		$("#left").append(fg);
-	}
-	for (var i = 6; i < grade.default.length; i++) {
-		var fg = $("<div></div>").addClass("form-group");
-		var label = $("<label></label>").addClass("control-label");
-		var box = $("<input>").addClass("form-control")
-		            .attr("type", "text").attr("data-subject", i)
-		            .attr("maxlength","4");
-		if (values != null) { $(box).val(values[i]) }
-		$(label).text(grade.default[i].subject);
-		$(fg).append(label).append(box);
-		$("#right").append(fg);
+	$("#g").attr("class","").text("Welcome");
+	for (var h = 1; h < 4; h++) {
+		$('#pane-'+h).remove();
+		$('#app .row').append($("<div></div>").addClass('col-lg-4').attr('id','pane-'+h));
 	}
 
-	$("input").on("change paste keyup", function(){ state.change(this) });
+	var boxes = 0;
+	var drawInput = function(i){
+		var pane = (i < 4) ? 1 : (i < 8) ? 2 : 3;
+		var group = $("<div></div>").addClass("subject");
+		var label = $("<span></span>")
+		var box = $("<input>").attr("type", "number").attr("data-subject", i);
+		if (values != null) { $(box).val(values[i]) }
+		$(label).text(grade.default[i].subject);
+		$(group).append(label).append(box);
+		$("#pane-"+pane).append(group);
+		boxes++;
+	}
+
+	for (var i = 0; i < 4; i++)
+		drawInput(i);
+	for (var i = 4; i < grade.default.length; i++)
+		drawInput(i);
+	if (grade.default.length > 8) {
+		for (var i = boxes; i < grade.default.length; i++)
+			drawInput(i);
+	} else {
+		$('#pane-3').remove();
+		$('#pane-1').addClass('col-lg-offset-2');
+	}
+
+	$("input").on("change paste keyup input", function(e){
+		if ($(this).val().length > 4) {
+			var trimmed = $(this).val().slice(0,4);
+			$(this).val(trimmed);
+		}
+		state.change(this);
+		//hopToNext(this);
+	});
 	if (values != null) { calculate() }
 }
 
+function hopToNext(from){
+	var grade = $(from).val();
+	if (grade.length == 4 && !isInvalidGrade(parseFloat(grade))) {
+		var boxes = $('input'),
+			lastBox = parseInt($(boxes[boxes.length-1]).attr('data-subject')),
+			fromBox = parseInt($(from).attr("data-subject"));
+		
+		if (fromBox != lastBox) {
+			$(from).blur();
+			$('input[data-subject='+(fromBox+1)+']').focus();
+		}
+	}
+}
+
+function isInvalidGrade(value) {
+	var grade = parseFloat(value), invalid = false;
+
+	// Contains characters other than numbers and period
+	if (/[^0-9\.]+/.test(value))
+		invalid = true;
+	// Out of range
+	else if (grade < 1 || grade > 5)
+		invalid = true;
+	// Indivisible by 0.25 for grades greater than 3.00
+	else if (grade < 3 && (grade % 0.25 != 0))
+		invalid = true;
+	// Between 3, 4, and 5
+	else if ((grade > 3 && grade < 4) || (grade > 4 && grade < 5))
+		invalid = true;
+
+	return invalid;
+}
 function calculate() {
-	var isInvalidGrade = function(value) {
-		var grade = parseFloat(value), invalid = false;
-
-		// Contains characters other than numbers and period
-		if (/[^0-9\.]+/.test(value))
-			invalid = true;
-		// Out of range
-		else if (grade < 1 || grade > 5)
-			invalid = true;
-		// Indivisible by 0.25 for grades greater than 3.00
-		else if (grade < 3 && (grade % 0.25 != 0))
-			invalid = true;
-		// Between 3, 4, and 5
-		else if ((grade > 3 && grade < 4) || (grade > 4 && grade < 5))
-			invalid = true;
-
-		return invalid;
-	};
-	
 	var total = 0, units = 0, gwa;
 
 	$("input").each(function(){
@@ -207,13 +271,12 @@ function calculate() {
 
 	gwa = total / units;
 	if (total == 0)
-		$("#g").attr("class","").text("GWA: (incomplete data)");
+		$("#g").attr("class","").text("Welcome");
 	else if (total == -1)
-		$("#g").attr("class","err").text("Invalid data.");
+		$("#g").attr("class","err").text("Error");
 	else {
+		$('#g').removeAttr("class").text(gwa.toPrecision(4));
 		if (gwa <= 1.500 && gwa != 0)
-			$("#g").attr("class","dl").text("GWA: " + gwa.toPrecision(4) + ", congrats!");
-		else
-			$("#g").attr("class","").text("GWA: " + gwa.toPrecision(4));
+			$("#g").addClass('dl');
 	}
 }
