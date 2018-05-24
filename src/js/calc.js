f86bb767a7b39b632e4c7c6311140793712bb572
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-24 15:55:38 +0000

    Deploy illustra/gwa to github.com/illustra/gwa.git:gh-pages

---
+++ src/js/calc.js
@@ -0,0 +1,115 @@
+/**
+    @file calc.js
+    @description Where the magic happens!
+    @author Jared Dantis (@aureljared)
+    @license GPLv2
+*/
+
+var calc = {
+    /**
+        Calculates grade average. Get it? calc.ulate? :-(
+
+        @returns {String} The computed average, truncated to 10 decimal places
+    */
+    ulate: function() {
+        var totalUnits = 0, total = 0, isGpa = state.get("isGpa"),
+            subjs = subjects.get();
+
+        // Get weighted sum
+        for (var i = 0; i < subjs.length; i++) {
+            // Get units
+            var units = subjs[i].units;
+            totalUnits += units;
+
+            // Get grade
+            var grade = state.getGrade(i);
+            if (isGpa)
+                grade = this.convertToGpa(grade);
+
+            // Multiply grade by units and add to sum
+            total += grade * units;
+        }
+
+        // Convert to string so we can truncate later
+        // since we're not supposed to round the value
+        return "" + (total / totalUnits).toFixed(10);
+    },
+
+    /**
+        Converts grade value to GPA equivalent.
+        Conversion is based on the following page:
+
+        @param {Float} grade - The grade value
+        @returns {Float} The GPA equivalent
+    */
+    convertToGpa: function(grade) {
+        var equiv;
+
+        switch (parseFloat(grade)) {
+            case 1:
+                equiv = 4.0;
+                break;
+            case 1.25:
+                equiv = 3.7;
+                break;
+            case 1.5:
+                equiv = 3.3;
+                break;
+            case 1.75:
+                equiv = 3.0;
+                break;
+            case 2:
+                equiv = 2.7;
+                break;
+            case 2.25:
+                equiv = 2.3;
+                break;
+            case 2.5:
+                equiv = 2.0;
+                break;
+            case 2.75:
+                equiv = 1.7;
+                break;
+            case 3:
+                equiv = 1.3;
+                break;
+            case 4:
+                equiv = 1.0;
+                break;
+            default:
+                equiv = 0.0;
+        }
+
+        return equiv;
+    },
+
+    /**
+        Checks if an user-entered grade value is a valid grade.
+
+        @param {String} value - The user-entered grade value
+        @returns {Object}
+            The result of the validation (.result) and the
+            reason for invalidation (.reason), if any.
+    */
+    isValid: function(value) {
+        var grade = parseFloat(value), reason = "";
+
+        // Contains characters other than numbers and period
+        if (/[^0-9\.]+/.test(value))
+            reason = "contains invalid characters";
+        // Out of range
+        else if (grade < 1 || grade > 5)
+            reason = grade + " is out of range";
+        // Indivisible by 0.25 for grades greater than 3.00
+        else if (grade < 3 && (grade % 0.25 != 0))
+            reason = "no such grade";
+        // Between 3, 4, and 5
+        else if ((grade > 3 && grade < 4) || (grade > 4 && grade < 5))
+            reason = "no such grade";
+
+        return {
+            result: reason.length > 0,
+            reason: reason
+        };
+    }
+};
---
+++ src/js/widget.js
@@ -0,0 +1,140 @@
+/**
+    @file widget.js
+    @description Reusable components (table rows)
+    @author Jared Dantis (@aureljared)
+    @license GPLv2
+*/
+
+var widget = {
+    /**
+        Creates a table row element (tr) that contains a
+        subject, along with the grade and -/+ controls.
+
+        @param {Int} id
+            The subject ID, e.g. the index of the subject
+            in the array subjects.default.
+        @param {String} subjName  -  Subject name
+        @param {String} subjGrade  -  Saved subject grade
+        @returns {jQuery} The created subject row
+    */
+    newSubjectRow: function(id, subjName, subjGrade){
+        //  <tr> element that will contain everything
+        var row = $('<tr>').attr('data-subject', id),
+        //  <td> element that will contain the grade & subject name
+            lcol = $('<td>'),
+            subj = $('<p>').text(subjName),
+            grade = $('<h2>').addClass('num').text(subjGrade),
+        //  <td> element that will contain the +/- buttons
+            rcol = $('<td>').addClass('controls'),
+            plus = $('<div>').addClass('plus'),
+            minus = $('<div>').addClass('minus');
+
+        // Add plus/minus button actions
+        $(minus).click(function(){
+            // Get current grade
+            var subjId = $(this).parent().parent().attr('data-subject'),
+                grade = state.getGrade(parseInt(subjId));
+
+            // Increment if possible
+            if (grade > 1) {
+                if (grade > 3)
+                    grade -= 1;
+                else
+                    grade -= 0.25;
+            }
+
+            // Update grade
+            state.setGrade(subjId, grade);
+            $('tr[data-subject='+subjId+'] h2').text(grade.toFixed(2));
+
+            // Recalculate
+            app.calculate();
+        });
+        $(plus).click(function(){
+            // Get current grade
+            var subjId = $(this).parent().parent().attr('data-subject'),
+                grade = state.getGrade(parseInt(subjId));
+
+            // Decrement if possible
+            if (grade < 5) {
+                if (grade > 2.75)
+                    grade += 1;
+                else
+                    grade += 0.25;
+            }
+
+            // Update grade
+            state.setGrade(subjId, grade);
+            $('tr[data-subject='+subjId+'] h2').text(grade.toFixed(2));
+
+            // Recalculate
+            app.calculate();
+        });
+
+        // Allow manual grade entry on grade click
+        $(grade).click(function(){
+            var subjId = $(this).parent().parent().attr('data-subject');
+            app.promptGrade(subjId);
+        });
+
+        // Put everything together
+        $(lcol).append(subj).append(grade);
+        $(rcol).append(minus).append(plus);
+        $(row).append(lcol).append(rcol);
+        return row;
+    },
+
+    /**
+        Creates a table row element (tr) that contains
+        input boxes for a custom subject and its units.
+
+        @param {Int} id
+            The subject ID, e.g. the index of the subject
+            in the array subjects.default.
+        @param {String} subjName  -  Subject name
+        @param {String} subjUnits  -  Subject units
+        @returns {jQuery} The created subject row
+    */
+    newCustomSubject: function(subjName, subjUnits) {
+        var row = $('<tr>'),
+            del = $('<td>').addClass('subject-delete'),
+            name = $('<td>').addClass('subject-name'),
+            unit = $('<td>').addClass('subject-units'),
+            nameInput = $('<input>').attr('type', 'text'),
+            unitInput = $('<input>').attr('type', 'text');
+
+        // on click delete button
+        $(del).click(function(){
+            var row = $(this).parent();
+
+            if ($(row).siblings().length > 0)
+                $(row).remove();
+            else {
+                // Don't delete the only row
+                // Instead, empty the content
+                var name = $(row).children('td.subject-name')
+                                 .children('input'),
+                    unit = $(row).children('td.subject-units')
+                                 .children('input');
+                $(name).val('').blur();
+                $(unit).val('').blur();
+            }
+        });
+
+        // Name & unit textbox placeholders
+        $(nameInput).attr('placeholder', 'Subject');
+        $(unitInput).attr('placeholder', 'Units');
+
+        // Restore content, if any
+        if (subjName && subjUnits) {
+            $(nameInput).val(subjName);
+            $(unitInput).val(subjUnits);
+        }
+
+        // Put everything together
+        $(name).append(nameInput);
+        $(unit).append(unitInput);
+        $(row).append(del).append(name).append(unit);
+        return row;
+    }
+};
