72ce7a2ae76e190b05ca9922eb91a074a09d0f7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-26 12:09:08 +0000

    Deploy illustra/gwa to github.com/illustra/gwa.git:gh-pages

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 */
 
 // Must match state.current.version*
-var CACHE_VERSION = "pisaygwa-web-r16-v12.5.2";
+var CACHE_VERSION = "pisaygwa-web-r16-v12.5.3";
 
 // Install: Cache app files
 self.addEventListener('install', function(event) {
