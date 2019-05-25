9c2a14c980c1fce20bc2b46bb929b6bb37920933
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-25 11:54:40 +0000

    Deploy illustra/gwa to github.com/illustra/gwa.git:gh-pages

--- Gulpfile.js
+++ Gulpfile.js
@@ -23,33 +23,70 @@ gulp.task('css', () => {
     const unprefix = require('postcss-unprefix');
     del(['dist/css']);
 
-    return gulp.src('./src/less/_style.less')
-        .pipe(less())
-        .pipe(postcss([
-            unprefix(),
-            autoprefixer()
-        ]))
-        .pipe(csso())
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('./dist/css'));
+    const buildCSS = function(src, dst) {
+        return gulp.src(`./src/less/${src}.less`)
+            .pipe(less())
+            .pipe(postcss([
+                unprefix(),
+                autoprefixer()
+            ]))
+            .pipe(csso())
+            .pipe(rename(`${dst}.css`))
+            .pipe(gulp.dest('./dist/css'));
+    };
+
+    buildCSS('_style', 'style');
+    return buildCSS('dialogs/_base', 'dialog');
 });
+
+function getDateStamp() {
+    const dObj = new Date(Date.now()), year = dObj.getFullYear();
+    let month = dObj.getMonth() + 1, day = dObj.getDate();
+    if (month < 10) month = '0' + month;
+    if (day < 10) day = '0' + day;
+    return ' build ' + year + month + day;
+}
 
 // Minify JS files
 gulp.task('js', () => {
-    const babel = require('gulp-babel');
-    del(['dist/js/script.js']);
+    const babel = require('gulp-babel'),
+        replace = require('gulp-replace');
 
+    del(['dist/js/script.js']);
     return gulp.src([
             './src/js/*.js',
             './src/js/dialogs/*.js'
         ])
         .pipe(concat('script.js'))
+        .pipe(replace(/(version: ".*)(")/, '$1' + getDateStamp() + '$2'))
         .pipe(babel({
             presets: ['@babel/preset-env']
         }))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
 });
+
+// Minify and strip debug
+gulp.task('js-strip', () => {
+    const babel = require('gulp-babel'),
+        replace = require('gulp-replace'),
+        strip = require('gulp-strip-debug');
+
+    del(['dist/js/script.js']);
+    return gulp.src([
+        './src/js/*.js',
+        './src/js/dialogs/*.js'
+    ])
+        .pipe(concat('script.js'))
+        .pipe(replace(/(version: ".*)(")/, '$1' + getDateStamp() + '$2'))
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(strip())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
 
 // Minify SVG files
 gulp.task('svg', () => {
@@ -78,7 +115,7 @@ gulp.task('html', () => {
 });
 
 // Generate service worker
-gulp.task('sw', gulp.series((callback) => {
+gulp.task('sw', gulp.series(() => {
     const workbox = require('workbox-build');
     return workbox.generateSW({
         globDirectory: '.',
@@ -124,6 +161,8 @@ gulp.task('sw', gulp.series((callback) => {
 
 // Gulp task to minify all files
 gulp.task('assets', gulp.parallel('css', 'js', 'html'));
-gulp.task('assets-svg', gulp.parallel('assets', 'svg'));
+gulp.task('html-sw', gulp.series('html', 'sw'));
+gulp.task('css-sw', gulp.series('css', 'sw'));
+gulp.task('js-sw', gulp.series('js', 'sw'));
 gulp.task('default', gulp.series('assets', 'sw'));
-gulp.task('all', gulp.series('assets-svg', 'sw'));
+gulp.task('release', gulp.series('html', 'css', 'js-strip', 'svg', 'sw'));
